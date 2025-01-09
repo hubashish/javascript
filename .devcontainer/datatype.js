3d39adbf0bd79b2e0aeaa147731fc9d8d0298243
Nagesh Giri <140241817+hubashish@users.noreply.github.com>
2025-01-09 11:24:13 +0000

    datatypes2

--- .devcontainer/datatype.js
+++ .devcontainer/datatype.js
@@ -1,11 +1,24 @@
-let score =55
-console.log(typeof score);
-console.log(typeof(score));
+// let score =55
+// // console.log(typeof score);
+// // console.log(typeof(score));
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
+// let valueInNumber = Number(score)
+// // console.log(typeof valueInNumber);
 
-let isLoggedIn = "Nagesh"
+// let isLoggedIn = "Nagesh"
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// // console.log(booleanIsLoggedIn)
+
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// // console.log( typeof stringNumber);
+
+// let value = 54
+// let negValue = -value
+// // console.log(negValue);
+
+let str1 = "Hello"
+let str2 = " Nagesh"
+let str3 = str1+str2
+comsole.log(str3);
