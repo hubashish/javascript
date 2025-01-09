9495c182f5232cfb5cd1ae5a1622aa3b5e69beed
Nagesh Giri <140241817+hubashish@users.noreply.github.com>
2025-01-09 06:26:11 +0000

    datatypes

---
+++ .devcontainer/datatype.js
@@ -0,0 +1,11 @@
+let score =55
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+
+let isLoggedIn = "Nagesh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
