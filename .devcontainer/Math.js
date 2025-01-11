15608283e549ceb9c20f1895c3e9cc5cc69dba8a
Nagesh Giri <140241817+hubashish@users.noreply.github.com>
2025-01-11 05:48:00 +0000

    Maths

---
+++ .devcontainer/Math.js
@@ -0,0 +1,12 @@
+console.log(Math);
+console.log(Math.round(4.58));
+console.log(Math.ceil(4.8));
+console.log(Math.floor(5.8));
+console.log(Math.abs(-8));
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10) +1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max-min+1))+min)
