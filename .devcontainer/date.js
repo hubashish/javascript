9266617bdc9599ae5906a0a6788f953f17649f39
Nagesh Giri <140241817+hubashish@users.noreply.github.com>
2025-01-11 08:39:50 +0000

    Date

---
+++ .devcontainer/date.js
@@ -0,0 +1,15 @@
+let myDate = new Date()
+console.log(myDate.toString());
+console.log(myDate.toDateString);
+console.log(myDate.toLocaleDateString);
+console.log(typeof myDate);
+
+let myCreateDate = new Date("01-05-2025")
+console.log(myCreatedDate.toLocaleDateString());
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth+1());
+console.log(newDate.getDate());
+console.log(newDate.getHours());
+console.log(newDate.getDay());
